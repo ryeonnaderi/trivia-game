28b8a11766c6152f783b4a759901d6e4f7e5362d
ryeon <ryeonnaderi@gmail.com>
2018-06-28 17:58:59 -0400

    adding the questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+var score = 0;
+
+// make a for loop that goes through the different questions
+
+console.log (question);
+// have a time limit for about 15 sec
+// if they get the answer right then their score goes up by 1 and shows the gif of the correct answer 
+// but if they get it wrong then their score  stays as zero and shows the correct answer and alerts better luck next time
