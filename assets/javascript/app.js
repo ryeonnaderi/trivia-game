c488aa27ed087f4f31bd0dd6c6b39d2f7945158c
ryeon <ryeonnaderi@gmail.com>
2018-06-29 17:13:08 -0400

    added the psudo code and redoing the questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,60 @@
 var score = 0;
 
 // make a for loop that goes through the different questions
+// var question = [" who is batman."," what was the first successful cloned animal."," what is the official currency of china"," what does the phrase cul -de - sac literally translates to","who created the super heroes and super villains"," before creating hot mail, the 2 founders worked at what company","Which famous math equation remains unsolved?"," when was snapchat made"," where was chocolate made"," where were noodles invented"]
+var question = [{
+        question: "Who is batman",
+        answers: {
+            a: "bruce wayne",
+            b: "Bruce banner",
+            c: "the flash"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What was the first cloned animal",
+        answers: {
+            a: "bear",
+            b: "pig",
+            c: "sheep"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "what is the official currency of china",
+        answers: {
+            a: "yen",
+            b: "Renminbi",
+            c: "peso"
+        },
+        correctAnswer: "b"
+    } {
+        question: " what does the phrase cul -de - sac literally translates to",
+        answers: {
+            a: "bottom of the sack",
+            b: "last cookie",
+            c: "apple sucks"
+        },
+        correctAnswer: "a"
+    }
 
-console.log (question);
+];
+for (var i = 0; i<question.length; i++)
+{
+     
+     
+
+}
+// when time reaches zero then switch to next question
+// make sure that the answer and the question match
+// user input on the button html button .val method radio buttons 
+// if  answer === correct answer then 
+// if the answer is correct then increase by 1 and show gif 
+// if they get it wrong then the correct answer shows up and the score remains the same 
+
+ 
+
+console.log(question);
 // have a time limit for about 15 sec
 // if they get the answer right then their score goes up by 1 and shows the gif of the correct answer 
 // but if they get it wrong then their score  stays as zero and shows the correct answer and alerts better luck next time
